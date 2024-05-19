d3d99c6795c2719afcd6480448dcf4446dec0460
Rojas Córsico Ivana Soledad <irojas@gmail.com>
2024-05-19 16:25:52 -0300

    handling error

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import { User } from '../models/user';
 
 @Injectable({
@@ -13,6 +13,21 @@ export class AuthService {
 
   createUser(user:User):Observable<any>
   {
-    return this.http.post(this.url, user)
+    return this.http.post(this.url, user).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // Error del lado del cliente, como un error de red, etc.
+      console.error('An error occurred:', error.error);
+    } else {
+      // Backend rechazó la petición y devuelve una respuesta con un código de estado.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Observable falla, retorna un mensaje de error genérico para el usuario.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
